511263dc5412567bd9d6476dcec3eb07a4393a03
GoesUp <gregor@gabrovsek.si>
2018-02-19 22:28:19 +0100

    4th problem.

---
+++ Problem004.py
@@ -0,0 +1,9 @@
+palindromi = []
+
+for i in reversed(range(100, 1000)):
+    for j in reversed(range(100, 1000)):
+        current = i*j
+        if str(current) == str(current)[::-1]:
+            palindromi += [current]
+
+print(max(palindromi))
