73d045c2f85f4648e4ec08d22601f7ba377ba698
GoesUp <gregor@gabrovsek.si>
2018-02-21 19:20:39 +0100

    Solved problem 12.

---
+++ Problem012.py
@@ -0,0 +1,29 @@
+import functools
+
+initial, count = 3, 3
+ended = False
+
+
+def getNumberOfDivisors(n):
+    factors = []
+    while not n == 1:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                factors += [i]
+                n = n // i
+                break
+
+    razlicne_potence = list(set(factors))
+    ponavljanje = [factors.count(x) for x in razlicne_potence]
+
+    return functools.reduce(lambda x, y: x * y, list(map(lambda w: w + 1, ponavljanje)))
+
+
+while not ended:
+    initial += count
+    count += 1
+    if getNumberOfDivisors(initial) > 500:
+        print(initial, count)
+        ended = True
+    else:
+        pass
