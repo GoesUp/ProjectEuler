1209bac757aefd837fffb184593e73107ba82943
GoesUp <gregor@gabrovsek.si>
2018-04-22 23:15:49 +0200

    Adapted and described problem 206.

--- Problem206.py
+++ Problem206.py
@@ -1,7 +1,15 @@
-low = int(10203040506070809 ** .5)
-static = "123456789"
-while True:
-    if str(low ** 2)[::2] == static:
-        print(low * 10)
-        break
-    low += 2
+def run() -> int:
+    low = int(10203040506070809 ** .5)
+    static = "123456789"
+    while True:
+        if str(low ** 2)[::2] == static:
+            return low * 10
+        low += 2
+
+
+if __name__ == "__main__":
+    from timeit import default_timer as timer
+
+    start = timer()
+    print("Solution:", run())
+    print("Duration:", timer() - start)
